79fc3cf4af7f6ac1d1f61aaba969602deb872031
yenarue <yenarue@gmail.com>
2017-11-27 00:02:18 +0900

    문의사항 게시판 - Contacts 모델 추가

---
+++ Node.js/Samples/FastCampus-Shop/models/contacts.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
+
+const ContactsSchema = new Schema({
+    title : String,
+    contents : Number,
+    author : String,
+    created_at : {
+        type : Date,
+        default : Date.now()
+    }
+});
+
+ContactsSchema.virtual('getDate').get(function() {
+    const date = new Date(this.created_at);
+    return {
+        year : date.getFullYear(),
+        month : date.getMonth() + 1,
+        day : date.getDate()
+    }
+});
+
+ContactsSchema.plugin( autoIncrement.plugin , { model : 'contacts' , field : 'seq' , startAt : 1 });
+module.exports = mongoose.model('contacts', ContactsSchema);
---
+++ Node.js/Samples/FastCampus-Shop/routes/contacts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Contacts = require('../models/contacts');
+
+// /contacts  글리스트
+// /contacts/write 글작성
+// /contacts/detail/:id  상세글보기
+// /contacts/edit/:id 글수정하기
+// /contacts/delete/:id 글삭제하기
+router.get('/', (req, res) => {
+    Contacts.find({}, (err, contacts) => {
+        res.render('/admin/')
+    });
+})
