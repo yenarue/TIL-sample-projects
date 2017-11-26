c70897fd6a1ee99d114b449017acf049f33bfb64
yenarue <yenarue@gmail.com>
2017-11-27 01:02:09 +0900

    문의하기 리스트 - 작성자 추가 / 작성일 수정

--- Node.js/Samples/FastCampus-Shop/models/contacts.js
+++ Node.js/Samples/FastCampus-Shop/models/contacts.js
@@ -18,7 +18,10 @@ ContactsSchema.virtual('getDate').get(function() {
     return {
         year : date.getFullYear(),
         month : date.getMonth() + 1,
-        day : date.getDate()
+        day : date.getDate(),
+        hour : date.getHours(),
+        minute : date.getMinutes(),
+        second : date.getSeconds(),
     }
 });
 
