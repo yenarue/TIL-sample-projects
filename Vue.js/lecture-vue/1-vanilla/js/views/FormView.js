b79fa1c2ba9a7c19dd53e3ab6ba3c7972ede3032
yenarue <yenarue@gmail.com>
2018-07-18 00:29:10 +0900

    [Vue.js] 검색어 입력시 삭제버튼이 보이도록 수정

--- Vue.js/lecture-vue/1-vanilla/js/views/FormView.js
+++ Vue.js/lecture-vue/1-vanilla/js/views/FormView.js
@@ -10,10 +10,20 @@ FormView.setup = function (el) {
     this.inputEl = el.querySelector('[type=text]')
     this.resetEl = el.querySelector('[type=reset]')
     this.showResetBtn(false)
+    this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function (show = true) {
     this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+FormView.bindEvents = function () {
+    this.inputEl.addEventListener('keyup', e => this.onKeyUp(e))
+}
+
+FormView.onKeyUp = function (e) {
+    this.showResetBtn(this.inputEl.value.length)
 }
 
 export default FormView
