051d083bd506bad6b2fbe33fb94a340fc7966472
yenarue <yenarue@gmail.com>
2018-08-01 22:32:59 +0900

    [Vue.js] 탭 추가 & 디폴트 탭 설정

--- Vue.js/lecture-vue/1-vanilla/js/controllers/MainController.js
+++ Vue.js/lecture-vue/1-vanilla/js/controllers/MainController.js
@@ -1,5 +1,6 @@
 import FormView from '../views/FormView.js';
 import ResultView from '../views/ResultView.js';
+import TabView from '../views/TabView.js';
 import SearchModel from '../models/SearchModel.js';
 
 const tag = '[MainController]';
@@ -11,7 +12,19 @@ export default {
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm());
 
+        TabView.setup(document.querySelector('#tabs'));
+
         ResultView.setup(document.querySelector('#search-result'));
+
+        this.selectedTab = '추천 검색어';
+        this.renderView();
+    },
+
+    // 그려주는 부분은 renderView()로 모두 위임
+    renderView() {
+        console.log(tag, 'renderView');
+        TabView.setActiveTab(this.selectedTab);
+        ResultView.hide();
     },
 
     search(query) {
