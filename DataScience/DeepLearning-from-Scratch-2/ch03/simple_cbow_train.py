13c1eeeb66d3e371600e30a4409cbac2980b281f
yenarue <yenarue@gmail.com>
2019-10-19 23:43:47 +0900

    [MachineLearning] add ch03

---
+++ DataScience/DeepLearning-from-Scratch-2/ch03/simple_cbow_train.py
@@ -0,0 +1,34 @@
+from common.util import preprocess, create_contexts_target, convert_one_hot
+from optimizer import Adam
+from common.trainer import Trainer
+from ch03.simple_cbow import SimpleCBOW
+
+window_size = 1
+hidden_size = 5
+batch_size = 3
+max_epoch = 1000
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+print(corpus)
+print(id_to_word)
+
+contexts, target = create_contexts_target(corpus, window_size=1)
+
+print(contexts)
+print(target)
+
+vocab_size = len(word_to_id)
+contexts = convert_one_hot(contexts, vocab_size)
+target = convert_one_hot(target, vocab_size)
+
+model = SimpleCBOW(vocab_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.plot()
+
+word_vecs = model.word_vecs
+for word_id, word in id_to_word.items():
+    print(word, word_vecs[word_id])
--- DataScience/DeepLearning-from-Scratch-2/common/util.py
+++ DataScience/DeepLearning-from-Scratch-2/common/util.py
@@ -90,3 +90,64 @@ def ppmi(C, verbose=False, eps=1e-8):
                 if cnt % (total // 100) == 0:
                     print('%.1f%% 완료' % (100 * cnt / total))
     return M
+
+def create_contexts_target(corpus, window_size=1):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for index in range(window_size, len(corpus) - window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[index + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
+
+def convert_one_hot(corpus, vocab_size):
+    '''원핫 표현으로 변환
+    :param corpus: 단어 ID 목록(1차원 또는 2차원 넘파이 배열)
+    :param vocab_size: 어휘 수
+    :return: 원핫 표현(2차원 또는 3차원 넘파이 배열)
+    '''
+    N = corpus.shape[0]
+
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
+
+def to_cpu(x):
+    import numpy
+    if type(x) == numpy.ndarray:
+        return x
+    return np.asnumpy(x)
+
+
+def to_gpu(x):
+    import cupy
+    if type(x) == cupy.ndarray:
+        return x
+    return cupy.asarray(x)
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
